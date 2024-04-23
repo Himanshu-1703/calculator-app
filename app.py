6d00fa329597f0eb8baf3a597438a59c8588f06c
Himanshu Arora <129079320+Himanshu-1703@users.noreply.github.com>
2024-04-23 17:35:22 +0530

    added feature for division

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from calc_func import do_addition, do_subtraction
+from calc_func import do_addition, do_subtraction, do_division
 from multiply import do_multiplication
 
 def main():
@@ -8,6 +8,7 @@ def main():
           1. Add
           2. Subtract
           3. Multiply
+          4. Division
           """)
     
     user_input = input("Select the function")
@@ -21,7 +22,9 @@ def main():
         result = do_subtraction(a,b)
     elif user_input == "3":
         result = do_multiplication(a,b)
-        
+    
+    elif user_input == "4":
+        result = do_division(a,b)    
     print('Result: ',result)
     
     
